e77b2b424b2469e6cb510b57434d829c58866c6d
Andranik Arshakyan <andranikarshakyan1997@gmail.com>
2022-02-18 16:30:03 +0300

    FInal commit, complete js, all project

--- js/index.js
+++ js/index.js
@@ -2,7 +2,24 @@ let feedBackPopup = document.querySelector('.contact-us');
 let modalOpen = document.querySelector('.modal');
 let modalClose = feedBackPopup.querySelector('.modal-close-mark');
 let form = document.querySelector('.contact-us-form');
+
 let userName =  feedBackPopup.querySelector('[name=username]');
+let email =  feedBackPopup.querySelector('[name=email]');
+let userMessage =  feedBackPopup.querySelector('[name=message]');
+
+let sliderControls = document.querySelectorAll('.slider-controls button');
+let sliderItems = document.querySelectorAll('.slider-item');
+
+for (let i = 0; i < sliderControls.length; i++) {
+  sliderControls[i].addEventListener('click', function () {
+    for (let j = 0; j < sliderControls.length; j++) {
+      sliderControls[j].classList.remove('button-current');
+      sliderControls[i].classList.add('button-current');
+      sliderItems[j].classList.remove('slider-current');
+      sliderItems[i].classList.add('slider-current');
+    }
+  });
+}
 
 
 modalOpen.addEventListener('click', function (evt) {
@@ -24,5 +41,10 @@ window.addEventListener('keydown', function (evt) {
 });
 
 form.addEventListener('submit', function (evt) {
+  if (!email.value || !userMessage.value || !userName.value) {
     evt.preventDefault();
+    alert('Все поля должны быть заполнены');
+  };
 });
+
+
